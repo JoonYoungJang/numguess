edb868f99d65c62f3c5823692fffc5839432d499
JoonYoungJang <wkdwnsdud35@gmail.com>
2023-02-02 12:25:05 +0900

    feat: Get user_name, guess from User using input()

--- numguess.py
+++ numguess.py
@@ -6,3 +6,11 @@ answer = randint(1, 100)
 
 # Print answer (for debugging)
 print(answer)
+
+# Get User's name, guess
+
+user_name = input('Hello, there! What is your name? ')
+guess = input(f'Hi, {user_name}. Guess the number(1-100): ')
+
+# print to check
+print(user_name, guess)
