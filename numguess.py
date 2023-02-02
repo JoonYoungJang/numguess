699487531fb01e0861c733e6541679a53b62b6c6
JoonYoungJang <wkdwnsdud35@gmail.com>
2023-02-02 12:44:25 +0900

    fix: Fix numguess.py

--- numguess.py
+++ numguess.py
@@ -4,13 +4,13 @@ from random import randint
 # Create answer range: 1 to 100 (integer)
 answer = randint(1, 100)
 
-# Print answer (for debugging)
-print(answer)
+# print debugging
+# print(answer)
 
 # Get User's name, guess
 
 user_name = input('Hello, there! What is your name? ')
-guess = input(f'Hi, {user_name}. Guess the number(1-100): ')
+guess = int(input(f'Hi, {user_name}. Guess the number(1-100): '))
 
 # print to check
 print(user_name, guess)
